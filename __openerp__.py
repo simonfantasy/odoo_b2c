8a461e73642f7384fe355f73e6fb006e7cd2a6a7
Simon <simonfantasy@gmail.com>
2016-09-24 10:49:41 +0000

    20160924- finishing b2c order lines with sale order and stock picking. adding views also

--- __openerp__.py
+++ __openerp__.py
@@ -1,8 +1,15 @@
 {
 	  'name': 'B2C Modification',
-		    'description': 'Extend the B2C Flexibility.',
-		      'author': 'Simon Feng',
-		        'depends': ['sale_stock'],
+	  'description':"""
+Extend the B2C Flexibility:
+
+1.销售订单增加b2c发货信息。
+
+2.出库单关联上述字段
+
+""",
+	   'author': 'Simon Feng',
+          'depends': ['sale_stock'],
 
 
 
--- b2c_stock_picking.py
+++ b2c_stock_picking.py
@@ -3,31 +3,8 @@
 from openerp import models, fields, api
 
 class StockPicking(models.Model):
-	_inherit = 'stock.picking'
-
-	@api.depends('move_lines')
-	def _compute_order_delivery(self):
-		for picking in self:
-		    sale_order = False
-		    for move in picking.move_lines:
-			if move.procurement_id.sale_line_id:
-			    sale_order = move.procurement_id.sale_line_id.order_id
-			    break
-			picking.sp_delivery_name = sale_order.order_delivery_name if sale_order else False
-#			picking.sp_delivery_address = sale_order.order_delivery_address if sale_order else False
-#			picking.sp_delivery_phone = sale_order.order_delivery_phone if sale_order else False
-	
-	def _search_order_delivery(self, operator, value):
-	        moves = self.env['stock.move'].search(
-			[('picking_id', '!=', False), ('procurement_id.sale_line_id.order_id', operator, value)]
-				)
-		return [('order_delivery_name', 'in', moves.mapped('picking_id').ids)]
-
-
-	sp_delivery_name = fields.Many2one(comodel_name='sale.order', string="B2C Order Delivery Name",
-			                   compute='_compute_order_delivery', search='_search_order_delivery')
-	sp_delivery_address = fields.Many2one(comodel_name='sale.order', string="B2C Order Delivery Address",
-                                           compute='_compute_order_delivery', search='_search_order_delivery')
-	sp_delivery_phone = fields.Many2one(comodel_name='sale.order', string="B2C Order Delivery Phone",
-                                           compute='_compute_order_delivery', search='_search_order_delivery')
+    _inherit = 'stock.picking'
+    sp_delivery_name = fields.Char(related='sale_id.order_delivery_name',string="B2C Order Delivery Name",readonly=True)
+    sp_delivery_address = fields.Char(related='sale_id.order_delivery_address',string="B2C Order Delivery Address",readonly=True)
+    sp_delivery_phone = fields.Char(related='sale_id.order_delivery_phone',string="B2C Order Delivery Phone",readonly=True)
 
